e2f3fa972b63e974a6b1a8d2e6baea86bd3b7dad
mahesh2291 <gmahesh252525@gmail.com>
2025-03-03 16:27:12 -0500

    video bg-autoplay,header adjusted

--- src/Components/VideoBackground.jsx
+++ src/Components/VideoBackground.jsx
@@ -12,7 +12,7 @@ const trailer=useSelector((store)=>store.movie.trailer)
 
     return (
       <div className="w-screen">
-        <iframe className="w-screen aspect-video"  src={"https://www.youtube.com/embed/rUSdnuOLebE?si="+key} title="YouTube video player"  frameBorder="0" allow="accelerometer; autoplay:true; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen >
+        <iframe className="w-screen aspect-video"  src={"https://www.youtube.com/embed/"+key + "?autoplay=1&mute=1"} title="YouTube video player"  frameBorder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen >
         </iframe>
         </div>
     )
