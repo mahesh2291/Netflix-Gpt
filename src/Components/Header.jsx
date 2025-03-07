066f89795b89f3c0145dec099606d7df702cee0a
mahesh2291 <gmahesh252525@gmail.com>
2025-03-06 19:51:28 -0500

    Fixed logut bug

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,6 +9,9 @@ import { logo } from "../utils/constants";
 
 const Header=() =>{
   const isGptEnabled=useSelector((store)=>store.user.gpt)
+
+  const user=useSelector((store)=>store.user.user)
+
     const navigate=useNavigate()
 
     const dispatch=useDispatch()
@@ -48,12 +51,14 @@ useEffect(()=>{
     return (
         <div className="absolute bg-gradient-to-b from-black w-screen flex justify-between" >
             <img className="w-40 mt-5 z-50 ml-20" alt="Logo" src={logo} />
-            <div className="flex">
-              <button className="text-white h-10 p-1 mt-10  rounded-md  z-50  mr-7 bg-lime-500" onClick={()=>dispatch(gptToggle())}>{isGptEnabled? 'HomePage' : 'Gpt Search'  }</button>
-                <img className="w-12 h-12 mt-10 mr-10" src="https://i.pinimg.com/564x/1b/a2/e6/1ba2e6d1d4874546c70c91f1024e17fb.jpg" />
-                <button onClick={()=>handleSignOut()} className="ml-2 mr-10 z-50 p-1 pb-2 text-white mt-12 border-2 h-7 ">Logout</button>
-                
-            </div>
+           {
+            user &&  <div className="flex">
+            <button className="text-white h-10 p-1 mt-10  rounded-md  z-50  mr-7 bg-lime-500" onClick={()=>dispatch(gptToggle())}>{isGptEnabled? 'HomePage' : 'Gpt Search'  }</button>
+              <img className="w-12 h-12 mt-10 mr-10" src="https://i.pinimg.com/564x/1b/a2/e6/1ba2e6d1d4874546c70c91f1024e17fb.jpg" />
+              <button onClick={()=>handleSignOut()} className="ml-2 mr-10 z-50 p-1 pb-2 text-white mt-12 border-2 h-7 ">Logout</button>
+              
+          </div>
+           }
        
         </div>
        
